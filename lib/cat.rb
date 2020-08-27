87339568b6a2c857366a57c0d9929cf745da15cf
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-08-27 17:35:40 -0400

    built three classes in seperate files and use them in unison

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 class Cat
-  # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all_cats = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all_cats.push(self)
+  end
+
+  def self.all
+    return @@all_cats
+  end
+
+
 end
+
+# my_cat = Cat.new("monks", "hyrum")
+# puts my_cat.owner
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,48 @@
+require_relative './cat.rb'
+require 'pry'
+
 class Owner
-  # code goes here
+
+  attr_reader :name, :species
+
+  @@all_the_owners = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all_the_owners.push(self)
+  end
+
+  def say_species
+    return "I am a #{self.species}."
+  end
+
+  def self.all
+    return @@all_the_owners
+  end
+
+  def self.count
+    return @@all_the_owners.length
+  end
+
+  def self.reset_all
+    @@all_the_owners = []
+  end
+
+  def cats
+    Cat.all.select do |kat|
+      kat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+
+  def buy_cat(cat_name)
+    Cat.new(cat_name, self)
+  end
+
 end
